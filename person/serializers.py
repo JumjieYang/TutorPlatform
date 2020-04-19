be3c1904e44114f5202100cbcfaecdcd4b68aba9
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-04-20 01:02:49 +0800

    fix password visibility

--- person/serializers.py
+++ person/serializers.py
@@ -7,9 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password')
-        # extra_kwargs = {
-        #     'password': {'write_only': True},
-        # }
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
         user = User(username=validated_data['username'])
