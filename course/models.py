d39f982fe9daa3fa61b87e3df05736127e1dbfd8
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-04-23 18:29:42 +0800

    add support to upload course image

--- course/models.py
+++ course/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from django.db.models import CASCADE
 
 
+def custom_upload_path(instance, filename):
+    class_name = instance.__class__.__name__.lower()
+    return "{}/{}-{}/{}".format(class_name + "s", class_name, instance.pk, filename)
+
+
 # Create your models here.
-
-
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
     subject = models.CharField(max_length=4)
@@ -16,6 +19,7 @@ class Course(models.Model):
     description = models.CharField(max_length=1000)
     isAvailable = models.BooleanField(default=False)
     price = models.FloatField(default=0)
+    image = models.ImageField(upload_to=custom_upload_path, null=True)
     tutor = models.ForeignKey(User, on_delete=CASCADE, null=True)
 
 
@@ -25,4 +29,3 @@ class ShoppingCart(models.Model):
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, to_field='id')
     number = models.IntegerField(default=0)
     total = models.IntegerField(default=0)
-
