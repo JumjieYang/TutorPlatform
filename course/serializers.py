4e048e8344695a48e7272e0ea08176cf0bca28c1
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-04-25 13:50:59 +0800

    add tutorserializer

--- course/serializers.py
+++ course/serializers.py
@@ -1,12 +1,19 @@
 from course.models import Course, ShoppingCart
 from rest_framework import serializers
 
+from person.models import Profile
+
+
+class TutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('firstName','lastName','image')
 
 class CourseSerializer(serializers.ModelSerializer):
+    tutor = TutorSerializer(read_only=True)
     class Meta:
         model = Course
         fields = '__all__'
-
 
 
 
