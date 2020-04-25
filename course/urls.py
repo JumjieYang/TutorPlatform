37de6627d8ecd55aa008fcf559a761d565103a24
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-04-25 20:37:05 +0800

    rewrite api

--- course/urls.py
+++ course/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('course/', CourseCreateView.as_view(),name='course_create'),
     path('course/<int:pk>', CourseDetail.as_view(),name='course_detail'),
+    path('courses/', CourseList.as_view(),name='course_all'),
     path('courses/<int:pk>', CourseList.as_view(),name='course_list'),
     path('carts/', CreateCart.as_view(),name='cart_list'),
     path('carts/', CartDetail.as_view(),name='cart_detail'),
