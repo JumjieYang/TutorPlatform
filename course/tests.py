be35851c126dee0947cf88465df52a98e2c4eeab
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-04-26 15:21:30 +0800

    remove unused code

--- course/tests.py
+++ course/tests.py
@@ -88,7 +88,6 @@ class Course_Test(APITestCase):
             'user': 1,
             'course': self.course.id
         }
-        print(data)
         response = self.client.post(url, data)
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
         response = response.json()
